24e2dd609135955762b0c69f0faa86c34be913d2
Grant Pezeshki <grant@panix.com>
2020-03-30 23:35:02 -0400

    rough start to spec #1 for map

--- map1.js
+++ map1.js
@@ -1,14 +1,25 @@
-let geoDataURL = "../CD_simplified_wPM.geojson";
+let geoDataURL = "../ZCTA.geo.json";
+let covidDataURL = "../FakeZCTAData.csv";
 let geoRequest = new XMLHttpRequest();
+let covidRequest = new XMLHttpRequest();
 let geoObj = {};
+let covidObj = {};
 let viewObj = {};
 
 geoRequest.open('GET',geoDataURL);
 geoRequest.responseType = 'json';
 geoRequest.send();
 
+covidRequest.open('GET',covidDataURL);
+covidRequest.responseType = 'text';
+covidRequest.send();
+
 geoRequest.onload = function(){
     geoObj = geoRequest.response;
+}
+
+covidRequest.onload = function(){
+    covidObj = covidRequest.response;
 }
 
 let spec = "map1.vl.json";
