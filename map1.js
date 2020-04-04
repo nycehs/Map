c3c9d31bee9c23fe5ca51e7fcb08c2d65c88beb0
Grant Pezeshki <grant@panix.com>
2020-04-04 01:36:21 -0400

    Cleaned up the code for map1 embed, updated geofiles

--- map1.js
+++ map1.js
@@ -1,29 +1,19 @@
-let geoDataURL = "../ZCTA.geo.json";
+let geoDataURL = "../zcta.topo.json";
 let covidDataURL = "../FakeZCTAData.csv";
-let geoRequest = new XMLHttpRequest();
-let covidRequest = new XMLHttpRequest();
 let geoObj = {};
 let covidObj = {};
 let viewObj = {};
-
-geoRequest.open('GET',geoDataURL);
-geoRequest.responseType = 'json';
-geoRequest.send();
-
-covidRequest.open('GET',covidDataURL);
-covidRequest.responseType = 'text';
-covidRequest.send();
-
-geoRequest.onload = function(){
-    geoObj = geoRequest.response;
-}
-
-covidRequest.onload = function(){
-    covidObj = covidRequest.response;
-}
-
 let spec = "map1.vl.json";
-  vegaEmbed('#map', spec).then(function(result) {
+const opt = {"renderer":"svg"};
+const el = document.getElementById('map');
+
+//this older version imports the data from URL in the spec
+const specOld = "map1.vl.json";
+function mapCreate() {
+vegaEmbed('#map', spec, opt).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     viewObj = result.view;
   }).catch(console.error);
+}
+
+mapCreate();
