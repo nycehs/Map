f9451a9712ed76eec6fc0b7a704a8a59324a546c
Grant Pezeshki <grant@panix.com>
2020-04-06 16:07:24 -0400

    Map chooser is implemented; embed css fixed for chooser

--- map1.js
+++ map1.js
@@ -3,17 +3,47 @@ let covidDataURL = "../FakeZCTAData.csv";
 let geoObj = {};
 let covidObj = {};
 let viewObj = {};
-let spec = "map1.vl.json";
+let countSpec = "map1.vl.json";
+let rateSpec = "mapRate.vl.json";
+let percentSpec = "mapPercent.vl.json";
+let deathSpec = "";
+let radios ;
+
+
 const opt = {"renderer":"svg"};
 const el = document.getElementById('map');
 
-//this older version imports the data from URL in the spec
-const specOld = "map1.vl.json";
-function mapCreate() {
-vegaEmbed('#map', spec, opt).then(function(result) {
+// this code listens to the form with map chooser; must run after DOM loads
+window.onload =listenRadios;
+
+function listenRadios() {
+  radios = document.querySelectorAll('input[type=radio][name="mapRadioGroup"]');
+  radios.forEach(radio => radio.addEventListener('change', () => {
+    if (radio.value==='Case Count') {countMapCreate()}
+    else if (radio.value==='Case Rate') {rateMapCreate()}
+    else {percentMapCreate()}  // for if chosenField is Percent Positive
+    ;
+  }));
+}
+  
+//this function creates the map
+function countMapCreate() {
+vegaEmbed('#map', countSpec, opt).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     viewObj = result.view;
   }).catch(console.error);
 }
+function rateMapCreate() {
+  vegaEmbed('#map', rateSpec, opt).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+      viewObj = result.view;
+    }).catch(console.error);
+  }
+  function percentMapCreate() {
+    vegaEmbed('#map', percentSpec, opt).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        viewObj = result.view;
+      }).catch(console.error);
+    }
 
-mapCreate();
+countMapCreate();
