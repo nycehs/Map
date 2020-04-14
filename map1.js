8f8df973dd465d0aa37851b10a946ab8d95cb15a
Matthew <mmontesano@health.nyc.gov>
2020-04-14 09:57:31 -0400

    Pre-launch assembly
    Updated FAKE_ZCTA_DATA
    Revised embed2 layout
    Added mapDeathCount and mapDeathRate
    Revised colors slightly so that counts and rates are each consistent

--- map1.js
+++ map1.js
@@ -6,7 +6,8 @@ let viewObj = {};
 let countSpec = "map1.vl.json";
 let rateSpec = "mapRate.vl.json";
 let percentSpec = "mapPercent.vl.json";
-let deathSpec = "";
+let deathCountSpec = "mapDeathCount.vl.json";
+let deathRateSpec = "mapDeathRate.vl.json";
 let radios ;
 
 
@@ -19,26 +20,44 @@ window.onload =listenRadios;
 function listenRadios() {
   radios = document.querySelectorAll('input[type=radio][name="mapRadioGroup"]');
   radios.forEach(radio => radio.addEventListener('change', () => {
-    if (radio.value==='Case Count') {countMapCreate()}
-    else if (radio.value==='Case Rate') {rateMapCreate()}
+    if (radio.value==='Case Rate') {rateMapCreate()}
+    else if (radio.value==='Case Count') {countMapCreate()}
+    else if (radio.value==='Death Count') {deathCountMapCreate()}
+    else if (radio.value==='Death Rate') {deathRateMapCreate()}
     else {percentMapCreate()}  // for if chosenField is Percent Positive
     ;
   }));
 }
   
 //this function creates the map
-function countMapCreate() {
-vegaEmbed('#map', countSpec, opt).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    viewObj = result.view;
-  }).catch(console.error);
-}
 function rateMapCreate() {
   vegaEmbed('#map', rateSpec, opt).then(function(result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
       viewObj = result.view;
     }).catch(console.error);
   }
+
+function countMapCreate() {
+vegaEmbed('#map', countSpec, opt).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    viewObj = result.view;
+  }).catch(console.error);
+}
+
+function deathCountMapCreate() {
+  vegaEmbed('#map', deathCountSpec, opt).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+      viewObj = result.view;
+    }).catch(console.error);
+  }
+
+  function deathRateMapCreate() {
+    vegaEmbed('#map', deathRateSpec, opt).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        viewObj = result.view;
+      }).catch(console.error);
+    }
+
   function percentMapCreate() {
     vegaEmbed('#map', percentSpec, opt).then(function(result) {
         // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -46,4 +65,4 @@ function rateMapCreate() {
       }).catch(console.error);
     }
 
-countMapCreate();
+rateMapCreate();
